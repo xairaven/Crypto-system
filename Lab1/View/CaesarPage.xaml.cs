2b8a2814d08fc785675adad9a40391ca3f89590e
xairaven <alexkovalevkpi@gmail.com>
2023-02-28 03:16:06 +0200

    (Lab 1) Added frames

---
+++ Lab1/View/CaesarPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Lab1.View;
+
+public partial class CaesarPage : Page
+{
+    public CaesarPage()
+    {
+        InitializeComponent();
+    }
+}
--- Lab1/View/MainWindow.xaml.cs
+++ Lab1/View/MainWindow.xaml.cs
@@ -20,14 +20,30 @@ namespace Lab1.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<Page> _pages;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            _pages = new List<Page>()
+            {
+                new CaesarPage(),
+                new Base64Page()
+            };
         }
 
         private void About_OnClick(object sender, RoutedEventArgs e)
         {
             new AboutWindow().Show();
+        }
+
+        private void CipherChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox list)
+            {
+                CipherFrame.Content = _pages[(int) list.SelectedItem];
+            }
         }
     }
 }
