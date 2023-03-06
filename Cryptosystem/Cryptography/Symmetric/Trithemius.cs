77480ebc2661d35a4bf7f1f7ab8870dbf28fc7c0
xairaven <alexkovalevkpi@gmail.com>
2023-03-06 02:33:48 +0200

    (Trithemius) Feat: Skeleton of cipher

---
+++ Cryptosystem/Cryptography/Symmetric/Trithemius.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Cryptosystem.Cryptography.Symmetric;
+
+public class Trithemius : Caesar
+{
+    public override string Encrypt(string message, params object[] keys)
+    {
+        Func<int, int> handler = ValidateAndGetHandler(TypeOfCoding.Encrypt, message, keys);
+
+        return TrithemiusCipher(message, handler);
+    }
+
+    public override string Decrypt(string message, params object[] keys)
+    {
+        Func<int, int> handler = ValidateAndGetHandler(TypeOfCoding.Decrypt, message, keys);
+        
+        return TrithemiusCipher(message, handler);
+    }
+    
+    private string TrithemiusCipher(string message, Func<int, int> handler)
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            char c = message[i];
+            
+            if (EscapeSequence.Contains(c))
+            {
+                sb.Append(c);
+                continue;
+            }
+            
+            sb.Append((char) ((c + handler(i)) % UnicodeCardinal));
+        }
+        
+        return sb.ToString();
+    }
+
+    private Func<int, int> ValidateAndGetHandler(TypeOfCoding method, string message, params object[] keys)
+    {
+        switch (keys.Length)
+        {
+            case 1:
+            {
+                if (keys[0] is not string motto)
+                    throw new ArgumentException("Wrong arguments! Operation ByMotto failed.");
+
+                return ByMotto(method, message, motto);
+            }
+            case 2:
+            {
+                if (keys[0] is not int a
+                    || keys[1] is not int b)
+                    throw new ArgumentException("Wrong arguments! Operation LinearEquation failed.");
+
+                return LinearEquation(method, a, b);
+            }
+            case 3:
+            {
+                if (keys[0] is not int a
+                    || keys[1] is not int b
+                    || keys[2] is not int c)
+                    throw new ArgumentException("Wrong arguments! Operation NonLinearEquation failed.");
+
+                return NonLinearEquation(method, a, b, c);
+            }
+            default:
+                throw new ArgumentException("Wrong arguments! Validation failed!");
+        }
+    }
+
+    private Func<int, int> LinearEquation(TypeOfCoding method, int a, int b)
+    {
+        var factor = method == TypeOfCoding.Encrypt ? 1 : -1;
+
+        return (int position) => factor * (a * position + b);
+    }
+
+    private Func<int, int> NonLinearEquation(TypeOfCoding method, int a, int b, int c)
+    {
+        var factor = method == TypeOfCoding.Encrypt ? 1 : -1;
+        
+        return (int position) => factor * (a * position * position + b * position + c);
+    }
+
+    private Func<int, int> ByMotto(TypeOfCoding method, string message, string motto)
+    {
+        var factor = method == TypeOfCoding.Encrypt ? 1 : -1;
+        
+        return (int position) =>
+        {
+            if (message.Length <= motto.Length) return motto[position];
+
+            var multiplier = (int) Math.Ceiling((decimal) message.Length / motto.Length);
+
+            var localMotto = string.Concat(Enumerable.Repeat(motto, multiplier));
+
+            return factor * localMotto[position];
+        };
+    }
+    
+    private enum TypeOfCoding
+    {
+        Encrypt,
+        Decrypt
+    }
+}
--- Cryptosystem/View/Symmetric/TrithemiusPage.xaml.cs
+++ Cryptosystem/View/Symmetric/TrithemiusPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Cryptosystem.Cryptography.Symmetric;
 
 namespace Cryptosystem.View.Symmetric;
 
@@ -9,6 +14,148 @@ public partial class TrithemiusPage : Page
     public TrithemiusPage(MainWindow window)
     {
         _textBox = window.MainTextArea;
+        _textBox.TextChanged += TextBoxOnTextChanged;
         InitializeComponent();
     }
+
+    private void KeyTypeCombo_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is not ComboBox) return;
+        
+        AreKeyBoxesEnabled();
+        IsButtonsEnabled(ValidateAllBoxes());
+    }
+
+    private void AreKeyBoxesEnabled()
+    {
+        if (KeyTypeCombo.SelectedItem is not KeyType type) return;
+        
+        ATermBox.IsEnabled = type is KeyType.Linear or KeyType.NonLinear;
+        BTermBox.IsEnabled = type is KeyType.Linear or KeyType.NonLinear;
+        CTermBox.IsEnabled = type is KeyType.NonLinear;
+        MottoBox.IsEnabled = type is KeyType.Motto;
+    }
+    
+    private void TermBoxEnabled(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (sender is not TextBox box) return;
+
+        if ((bool)e.NewValue)
+        {
+            box.Text = "0";
+            return;
+        }
+        
+        ClearIfDisabled(sender, e);
+    }
+    
+    private void ClearIfDisabled(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (sender is not TextBox box) return;
+        
+        if ((bool)e.NewValue == false) box.Text = "";
+    }
+    
+    private void TermBox_OnLostFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is not TextBox box) return;
+
+        if (box.Text.Trim().Equals("")) box.Text = "0";
+        
+        IsButtonsEnabled(ValidateAllBoxes());
+    }
+    
+    private void MottoBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        IsButtonsEnabled(ValidateAllBoxes());
+    }
+
+    private void TextBoxOnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        IsButtonsEnabled(ValidateAllBoxes());
+    }
+
+    private bool ValidateAllBoxes()
+    {
+        if (KeyTypeCombo.SelectedItem is not KeyType type) return false;
+
+        if (type is KeyType.Motto && MottoBox.Text.Trim().Equals("")) return false;
+
+        return !_textBox.Text.Trim().Equals("");
+    }
+    private void IsButtonsEnabled(bool value)
+    {
+        EncryptButton.IsEnabled = value;
+        DecryptButton.IsEnabled = value;
+    }
+
+    private void NumericOnly(object sender, TextCompositionEventArgs e)
+    {
+        e.Handled = IsTextNumeric(e.Text);
+    }
+    
+    private static bool IsTextNumeric(string str)
+    {
+        return !MyRegex().IsMatch(str);
+    }
+
+    [GeneratedRegex(@"^[0-9\-]+$")]
+    private static partial Regex MyRegex();
+
+    private void EncryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (KeyTypeCombo.SelectedItem is not KeyType type) 
+            throw new ApplicationException("Choose type of key");
+
+        if (type == KeyType.Motto)
+        {
+            _textBox.Text = new Trithemius().Encrypt(_textBox.Text, MottoBox.Text);
+            return;
+        }
+        
+        int.TryParse(ATermBox.Text, out var a);
+        int.TryParse(BTermBox.Text, out var b);
+
+        if (type == KeyType.Linear)
+        {
+            _textBox.Text = new Trithemius().Encrypt(_textBox.Text, a, b);
+            return;
+        }
+
+        int.TryParse(CTermBox.Text, out var c);
+
+        _textBox.Text = new Trithemius().Encrypt(_textBox.Text, a, b, c);
+    }
+    
+    private void DecryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (KeyTypeCombo.SelectedItem is not KeyType type) 
+            throw new ApplicationException("Choose type of key");
+
+        if (type == KeyType.Motto)
+        {
+            _textBox.Text = new Trithemius().Decrypt(_textBox.Text, MottoBox.Text);
+            return;
+        }
+        
+        int.TryParse(ATermBox.Text, out var a);
+        int.TryParse(BTermBox.Text, out var b);
+
+        if (type == KeyType.Linear)
+        {
+            _textBox.Text = new Trithemius().Decrypt(_textBox.Text, a, b);
+            return;
+        }
+
+        int.TryParse(CTermBox.Text, out var c);
+
+        _textBox.Text = new Trithemius().Decrypt(_textBox.Text, a, b, c);
+    }
+}
+
+public enum KeyType
+{
+    Linear,
+    NonLinear,
+    Motto
 }
