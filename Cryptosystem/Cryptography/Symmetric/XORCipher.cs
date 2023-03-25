5d292b1097bc5de06c80ef2785a69277f7baf552
xairaven <alexkovalevkpi@gmail.com>
2023-03-25 18:19:47 +0200

    (Symmetric ciphers) Done: XOR Cipher

--- Cryptosystem/Cryptography/Symmetric/XORCipher.cs
+++ Cryptosystem/Cryptography/Symmetric/XORCipher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Cryptosystem.Cryptography.Symmetric;
 
@@ -6,11 +7,37 @@ public class XORCipher : SymmetricCipher
 {
     public override string Encrypt(string message, params object[] keys)
     {
-        throw new System.NotImplementedException();
+        var seed = Validate(keys);
+
+        return Cipher(message, seed);
     }
 
     public override string Decrypt(string message, params object[] keys)
     {
-        throw new System.NotImplementedException();
+        var seed = Validate(keys);
+
+        return Cipher(message, seed);
+    }
+
+    private string Cipher(string message, int seed)
+    {
+        var random = new Random(seed);
+
+        var sb = new StringBuilder();
+        foreach (var с in message)
+        {
+            var gamma = (char) random.Next(1, UnicodeCardinal + 1);
+            sb.Append((char) (с ^ gamma));
+        }
+
+        return sb.ToString();
+    }
+
+    private int Validate(params object[] keys)
+    {
+        if (keys.Length != 1) throw new Exception("Wrong args");
+        if (keys[0] is not int key) throw new Exception("Key must be integer");
+
+        return key;
     }
 }
--- Cryptosystem/View/Symmetric/XORCipherPage.xaml.cs
+++ Cryptosystem/View/Symmetric/XORCipherPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Cryptosystem.Cryptography.Symmetric;
 
 namespace Cryptosystem.View.Symmetric;
 
@@ -9,7 +13,61 @@ public partial class XORCipherPage : Page
     public XORCipherPage(MainWindow window)
     {
         _textBox = window.MainTextArea;
-        
+        _textBox.TextChanged += TextBoxOnTextChanged;
         InitializeComponent();
+    }
+    
+    private void NumericOnly(object sender, TextCompositionEventArgs e)
+    {
+        e.Handled = IsTextNumeric(e.Text);
+    }
+    
+    private static bool IsTextNumeric(string str)
+    {
+        return !MyRegex().IsMatch(str);
+    }
+
+    [GeneratedRegex(@"^[0-9\-]+$")]
+    private static partial Regex MyRegex();
+
+    private void EncryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        int.TryParse(KeyBox.Text, out var key);
+        if (!Validate(key)) return;
+        
+        _textBox.Text = new XORCipher().Encrypt(_textBox.Text, key);
+    }
+
+    private void DecryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        int.TryParse(KeyBox.Text, out var key);
+        if (!Validate(key)) return;
+        
+        _textBox.Text = new XORCipher().Decrypt(_textBox.Text, key);
+    }
+    
+    private bool Validate(int key)
+    {
+        if (key != 0) return true;
+       
+        MessageBox.Show(
+            messageBoxText: "Key must be a number (or unequal to 0)",
+            caption: "Error",
+            button: MessageBoxButton.OK,
+            icon: MessageBoxImage.Error
+        );
+        return false;
+    }
+
+    private void KeyBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        EncryptButton.IsEnabled = !KeyBox.Text.Trim().Equals("") && !_textBox.Text.Equals("");
+        DecryptButton.IsEnabled = !KeyBox.Text.Trim().Equals("") && !_textBox.Text.Equals("");
+    }
+    
+    private void TextBoxOnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        EncryptButton.IsEnabled = !KeyBox.Text.Trim().Equals("") && !_textBox.Text.Equals("");
+        DecryptButton.IsEnabled = !KeyBox.Text.Trim().Equals("") && !_textBox.Text.Equals("");
     }
 }
