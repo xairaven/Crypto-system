0bd64a8dc09d2a46b30bd3d052d16eb8ad730b88
xairaven <alexkovalevkpi@gmail.com>
2023-03-27 14:28:59 +0300

    (Vernam) Feat: Done

--- Cryptosystem/Cryptography/Symmetric/Vernam.cs
+++ Cryptosystem/Cryptography/Symmetric/Vernam.cs
@@ -1,14 +1,52 @@
-﻿namespace Cryptosystem.Cryptography.Symmetric;
+﻿using System;
+using System.Text;
+
+namespace Cryptosystem.Cryptography.Symmetric;
 
 public class Vernam : SymmetricCipher
 {
     public override string Encrypt(string message, params object[] keys)
     {
-        throw new System.NotImplementedException();
+        var gamma = Validate(message.Length, keys);
+        
+        return VernamCipher(Mode.Encrypt, message, gamma);
     }
 
     public override string Decrypt(string message, params object[] keys)
     {
-        throw new System.NotImplementedException();
+        var gamma = Validate(message.Length, keys);
+        
+        return VernamCipher(Mode.Decrypt, message, gamma);
+    }
+
+    private string VernamCipher(Mode mode, string message, string gamma)
+    {
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < message.Length; i++)
+        {
+            var symbol = (mode == Mode.Encrypt) ? 
+                message[i] + gamma[i] : 
+                message[i] - gamma[i];
+
+            sb.Append((char) (symbol % UnicodeCardinal));
+        }
+
+        return sb.ToString();
+    }
+
+    private string Validate(int messageLength, params object[] keys)
+    {
+        if (keys.Length != 1) throw new Exception("Wrong args");
+        if (keys[0] is not string gamma) throw new Exception("Key must be integer");
+        if (gamma.Length != messageLength) throw new Exception("Message and pad lengths must be equal");
+            
+        return gamma;
+    }
+
+    private enum Mode
+    {
+        Encrypt,
+        Decrypt
     }
 }
--- Cryptosystem/View/Symmetric/VernamPage.xaml.cs
+++ Cryptosystem/View/Symmetric/VernamPage.xaml.cs
@@ -1,17 +1,111 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
 using System.Windows.Controls;
+using Cryptosystem.Cryptography.Symmetric;
+using Cryptosystem.Model;
 
 namespace Cryptosystem.View.Symmetric;
 
 public partial class VernamPage : Page
 {
     private readonly TextBox _textBox;
-    private FileInfo? _dictFileInfo;
+    private FileInfo? _fileInfo;
     
     public VernamPage(MainWindow window)
     {
         _textBox = window.MainTextArea;
-
+        _textBox.TextChanged += TextBoxOnTextChanged;
+        
         InitializeComponent();
+    }
+
+    private void EncryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var gamma = File.ReadAllLines(_fileInfo.FullName, Encoding.Unicode);
+
+        try
+        {
+            _textBox.Text = new Vernam().Encrypt(_textBox.Text, gamma);
+        }
+        catch (Exception exc)
+        {
+            MessageBox.Show(
+                messageBoxText: exc.Message,
+                button: MessageBoxButton.OK,
+                caption: "Error",
+                icon: MessageBoxImage.Error
+            );
+        }
+    }
+
+    private void DecryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var gamma = File.ReadAllLines(_fileInfo.FullName, Encoding.Unicode);
+
+        try
+        {
+            _textBox.Text = new Vernam().Decrypt(_textBox.Text, gamma);
+        }
+        catch (Exception exc)
+        {
+            MessageBox.Show(
+                messageBoxText: exc.Message,
+                button: MessageBoxButton.OK,
+                caption: "Error",
+                icon: MessageBoxImage.Error
+            );
+        }
+    }
+
+    private void OpenPad_OnClick(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            _fileInfo = new FileController().Open();
+        }
+        catch (Exception)
+        {
+            return;
+        }
+
+        if (!_fileInfo.Extension.Equals(".txt"))
+        {
+            MessageBox.Show(
+                messageBoxText: "Error! Filetype of dictionary must be txt.",
+                caption: "Wrong filetype!",
+                button: MessageBoxButton.OK,
+                icon: MessageBoxImage.Error);
+            return;
+        }
+
+        FilePath.Text = _fileInfo.Name;
+        FilePath.IsEnabled = true;
+        
+        IsButtonsEnabled();
+    }
+    
+    private void ClosePad_OnClick(object sender, RoutedEventArgs e)
+    {
+        _fileInfo = null;
+        FilePath.Text = "";
+        FilePath.IsEnabled = false;
+        
+        IsButtonsEnabled();
+    }
+    
+    private void TextBoxOnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        IsButtonsEnabled();
+    }
+
+    private void IsButtonsEnabled()
+    {
+        var isEnabled = !_textBox.Text.Equals("")
+                         && _fileInfo is not null;
+
+        EncryptButton.IsEnabled = isEnabled;
+        DecryptButton.IsEnabled = isEnabled;
     }
 }
