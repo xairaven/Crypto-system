3d9011965d69d14618a401b73d48660c9fde9cf1
xairaven <alexkovalevkpi@gmail.com>
2023-04-20 02:22:50 +0300

    (Symmetric ciphers) Feat: AES, DES, TripleDES

--- Cryptosystem/Cryptography/Symmetric/IStandardCipher.cs
+++ Cryptosystem/Cryptography/Symmetric/IStandardCipher.cs
@@ -22,12 +22,12 @@ public interface IStandardCipher
     public string Encrypt(CipherEnum type, string message, params object[] keys)
     {
         var provider = CryptoProvider(type, keys);
-        
-        var encryptor = provider.CreateEncryptor();
+
+        var encryptor = provider.CreateEncryptor(provider.Key, provider.IV);
 
         var memoryStream = new MemoryStream();
         var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
-
+        
         using (var writer = new StreamWriter(cryptoStream))
         {
             writer.Write(message);
@@ -48,14 +48,14 @@ public interface IStandardCipher
     public string Decrypt(CipherEnum type, string message, params object[] keys)
     {
         var provider = CryptoProvider(type, keys);
-        
+
         var dataMode = ValidateCastDataMode(keys[0]);
         
         var bytes = dataMode is DataMode.Base64 ? 
             Convert.FromBase64String(message) 
             : Convert.FromHexString(message);
 
-        var decryptor = provider.CreateDecryptor();
+        var decryptor = provider.CreateDecryptor(provider.Key, provider.IV);
 
         var memoryStream = new MemoryStream(bytes);
         var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
@@ -86,9 +86,12 @@ public interface IStandardCipher
         };
         
         cipher.Mode = ValidateCastCipherMode(keys[1], keys.Length);
+
         cipher.Key = Encoding.UTF8.GetBytes(ValidateCastKey(keys[2], type));
         cipher.IV = Encoding.UTF8.GetBytes(ValidateCastIV(keys[3]));
 
+        if (type is CipherEnum.DES && cipher.Mode is CipherMode.CFB) cipher.FeedbackSize = 8;
+        
         return cipher;
     }
 
@@ -104,8 +107,6 @@ public interface IStandardCipher
     {
         // Cipher mode
         if (m is not CipherMode mode) throw new ArgumentException("Wrong cipher mode");
-        if (length != 4 && mode is not (CipherMode.ECB or CipherMode.CTS))
-            throw new ArgumentException("IV is required");
 
         return mode;
     }
@@ -119,8 +120,8 @@ public interface IStandardCipher
         switch (cipher)
         {
             case CipherEnum.DES when bytes != 8:
-            case CipherEnum.AES when !new[] {8, 16, 24}.Contains(bytes):
-            case CipherEnum.TripleDES when bytes != 16:
+            case CipherEnum.AES when !new[] {16, 24, 32}.Contains(bytes):
+            case CipherEnum.TripleDES when !new[] {16, 24}.Contains(bytes):
                 throw new ArgumentException("Bad key size.");
         }
 
--- Cryptosystem/View/Symmetric/DESPage.xaml.cs
+++ Cryptosystem/View/Symmetric/DESPage.xaml.cs
@@ -1,12 +1,17 @@
-﻿using System.IO;
+﻿using System;
+using System.Text;
+using System.Windows;
 using System.Windows.Controls;
+using Cryptosystem.Cryptography.Symmetric;
+using Cryptosystem.Enum;
+
+using SysCrypto = System.Security.Cryptography;
 
 namespace Cryptosystem.View.Symmetric;
 
 public partial class DESPage : Page
 {
     private readonly TextBox _textBox;
-    private FileInfo? _fileInfo;
     
     public DESPage(MainWindow window)
     {
@@ -18,6 +23,118 @@ public partial class DESPage : Page
 
     private void TextBoxOnTextChanged(object sender, TextChangedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        IsButtonsEnabled();
+    }
+
+    private void EncryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (DataModeCombo.SelectedItem is not DataMode dataMode)
+            throw new Exception("Wrong data mode");
+
+        if (CipherModeCombo.SelectedItem is not SysCrypto.CipherMode cipherMode)
+            throw new Exception("Wrong cipher mode");
+        
+        if (IVBox.Text.Trim().Equals(""))
+        {
+            GenerateIV();
+        }
+
+        try
+        {
+            _textBox.Text = new DES().Encrypt(_textBox.Text, dataMode, cipherMode, 
+                KeyBox.Text.Trim(), IVBox.Text.Trim());
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show(
+                messageBoxText: exception.Message,
+                caption: "Error!",
+                button: MessageBoxButton.OK,
+                icon: MessageBoxImage.Error);
+        }
+    }
+
+    private void DecryptButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (DataModeCombo.SelectedItem is not DataMode dataMode)
+            throw new Exception("Wrong data mode");
+
+        if (CipherModeCombo.SelectedItem is not SysCrypto.CipherMode cipherMode)
+            throw new Exception("Wrong cipher mode");
+
+        if (IVBox.Text.Trim().Equals(""))
+        {
+            GenerateIV();
+        }
+        
+        
+        try
+        {
+            _textBox.Text = new DES().Decrypt(_textBox.Text, dataMode, cipherMode, 
+                KeyBox.Text.Trim(), IVBox.Text.Trim());
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show(
+                messageBoxText: exception.Message,
+                caption: "Error!",
+                button: MessageBoxButton.OK,
+                icon: MessageBoxImage.Error);
+        }
+    }
+
+    private void CipherModeChanged(object sender, SelectionChangedEventArgs e)
+    {
+        IsButtonsEnabled();
+    }
+
+    private void DataModeChanged(object sender, SelectionChangedEventArgs e)
+    {
+        IsButtonsEnabled();
+    }
+
+    private void KeyBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        IsButtonsEnabled();
+    }
+
+    private void IsButtonsEnabled()
+    {
+        var isEnabled = !_textBox.Text.Trim().Equals("")
+                        && !KeyBox.Text.Trim().Equals("")
+                        && CipherModeCombo.SelectedItem is not null
+                        && DataModeCombo.SelectedItem is not null;
+
+        EncryptButton.IsEnabled = isEnabled;
+        DecryptButton.IsEnabled = isEnabled;
+    }
+    
+    private void GenerateKeyButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        using var provider = SysCrypto.DES.Create();
+        provider.GenerateKey();
+        var bytes = provider.Key;
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            bytes[i] %= 128;
+        }
+        KeyBox.Text = Encoding.ASCII.GetString(bytes);
+    }
+
+    private void GenerateIVButton_OnClick(object sender, RoutedEventArgs e)
+    {
+       GenerateIV();
+    }
+
+    private void GenerateIV()
+    {
+        using var provider = SysCrypto.DES.Create();
+        provider.GenerateIV();
+        var bytes = provider.IV;
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            bytes[i] %= 128;
+        }
+        IVBox.Text = Encoding.ASCII.GetString(bytes);
     }
 }
