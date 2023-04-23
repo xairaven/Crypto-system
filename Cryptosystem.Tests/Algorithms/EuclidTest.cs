8c639a1c85ed24047401711633d77177240c9510
xairaven <alexkovalevkpi@gmail.com>
2023-04-23 03:45:14 +0300

    (Global) Tests: xUnit

---
+++ Cryptosystem.Tests/Algorithms/EuclidTest.cs
@@ -0,0 +1,27 @@
+﻿using Cryptosystem.Algorithms;
+
+namespace Cryptosystem.Tests.Algorithms;
+
+public class EuclidTest
+{
+    [Fact]
+    public void TestGCD1()
+    {
+        // Arrange
+        const int a = 122;
+        const int b = 10;
+        const int expected = 2;
+        
+        // Act
+        var result = Euclid.GCD(a, b);
+        
+        // Result
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void TestGCD2()
+    {
+        Assert.Equal(1, Euclid.GCD(133, 5));
+    }
+}
