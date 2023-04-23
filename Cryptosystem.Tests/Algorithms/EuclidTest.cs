ec6c6dfc0f7b75271a8ea29478309ec9fe47c37d
xairaven <alexkovalevkpi@gmail.com>
2023-04-24 01:27:51 +0300

    (Algorithms) Feat: Modular multiplicative inverse algorithm

--- Cryptosystem.Tests/Algorithms/EuclidTest.cs
+++ Cryptosystem.Tests/Algorithms/EuclidTest.cs
@@ -11,17 +11,41 @@ public class EuclidTest
         const int a = 122;
         const int b = 10;
         const int expected = 2;
-        
+
         // Act
         var result = Euclid.GCD(a, b);
-        
+
         // Result
         Assert.Equal(expected, result);
     }
-    
+
     [Fact]
     public void TestGCD2()
     {
         Assert.Equal(1, Euclid.GCD(133, 5));
+    }
+
+    [Fact]
+    public void TestGCD3()
+    {
+        Assert.Equal(1, Euclid.GCD(2000, 51));
+    }
+    
+    [Fact]
+    public void TestModularReverse1()
+    {
+        Assert.Equal(1451, Euclid.ModularReverse(51, 2000));
+    }
+    
+    [Fact]
+    public void TestModularReverse2()
+    {
+        Assert.Equal(4, Euclid.ModularReverse(3, 11));
+    }
+    
+    [Fact]
+    public void TestModularReverse3()
+    {
+        Assert.Equal(12, Euclid.ModularReverse(10, 17));
     }
 }
--- Cryptosystem/Algorithms/Euclid.cs
+++ Cryptosystem/Algorithms/Euclid.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cryptosystem.Algorithms;
 
-public class Euclid
+public static class Euclid
 {
     public static int GCD(int p, int q)
     {
@@ -8,5 +8,15 @@ public class Euclid
 
         var r = p % q;
         return GCD(q, r);
+    }
+    
+    public static int ModularReverse(int a, int mod)
+    {
+        for (var x = 1; x < mod; x++)
+        {
+            if (((a % mod) * (x % mod)) % mod == 1) return x;
+        }
+            
+        return 1;
     }
 }
