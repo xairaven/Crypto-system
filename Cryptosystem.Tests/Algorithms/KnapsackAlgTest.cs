e9a2b31918b009956d8d0d34020e3052363456ed
xairaven <alexkovalevkpi@gmail.com>
2023-04-24 01:28:20 +0300

    (Algorithms) Feat: Knapsack algorithm

---
+++ Cryptosystem.Tests/Algorithms/KnapsackAlgTest.cs
@@ -0,0 +1,49 @@
+﻿using Cryptosystem.Algorithms;
+
+namespace Cryptosystem.Tests.Algorithms;
+
+public class KnapsackAlgTest
+{
+    [Fact]
+    public void TestAlg1()
+    {
+        // Arrange
+        const int sum = 1161; 
+        var sequence = new int[] {43, 129, 215, 473, 903};
+
+        var expected = "10101";
+        
+        // Act
+        var result = KnapsackAlg.GetBytes(sum, sequence);
+        
+        // Result
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void TestAlg2()
+    {
+        var sequence = new int[] {43, 129, 215, 473, 903};
+        const int sum = 43;
+        const string expected = "10000";
+        Assert.Equal(expected, KnapsackAlg.GetBytes(sum, sequence));
+    }
+    
+    [Fact]
+    public void TestAlg3()
+    {
+        var sequence = new int[] {43, 129, 215, 473, 903};
+        const int sum = 1376;
+        const string expected = "00011";
+        Assert.Equal(expected, KnapsackAlg.GetBytes(sum, sequence));
+    }
+    
+    [Fact]
+    public void TestAlg4()
+    {
+        var sequence = new int[] {2, 3, 6, 13, 27, 52};
+        const int sum = 70;
+        const string expected = "110101";
+        Assert.Equal(expected, KnapsackAlg.GetBytes(sum, sequence));
+    }
+}
---
+++ Cryptosystem/Algorithms/KnapsackAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+
+namespace Cryptosystem.Algorithms;
+
+public static class KnapsackAlg
+{
+    public static string GetBytes(int S, int[] sequence)
+    {
+        if (!IsSorted(sequence)) Array.Sort(sequence);
+
+        var result = "";
+        for (int i = sequence.Length - 1; i >= 0; i--)
+        {
+            if (sequence[i] <= S)
+            {
+                S -= sequence[i];
+                result = "1" + result;
+            }
+            else
+            {
+                result = "0" + result;
+            }
+        }
+
+        if (S != 0) throw new EvaluateException("Problem is not solved!");
+        
+        return result;
+    }
+    
+    private static bool IsSorted(int[] sequence)
+    {
+        for (int i = 1; i < sequence.Length; i++)
+        {
+            if (sequence[i] < sequence[i - 1]) return false;
+        }
+
+        return true;
+    }
+}
