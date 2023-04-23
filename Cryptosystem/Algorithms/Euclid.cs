4a1b8d46d4df44ab33181bde19c8947c548f2a05
xairaven <alexkovalevkpi@gmail.com>
2023-04-23 03:46:21 +0300

    (Algorithms) Feat: Euclid's GCD algorithm

---
+++ Cryptosystem/Algorithms/Euclid.cs
@@ -0,0 +1,12 @@
+﻿namespace Cryptosystem.Algorithms;
+
+public class Euclid
+{
+    public static int GCD(int p, int q)
+    {
+        if (q == 0) return p;
+
+        var r = p % q;
+        return GCD(q, r);
+    }
+}
