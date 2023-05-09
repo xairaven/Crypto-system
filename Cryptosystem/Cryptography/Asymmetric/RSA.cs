ade0a19e3d4ced1e458235811b841a9402e0a2e6
xairaven <alexkovalevkpi@gmail.com>
2023-05-10 01:57:10 +0300

    (RSA) Feat: Encryption, Decryption, Generating PEM keys

--- Cryptosystem/Cryptography/Asymmetric/RSA.cs
+++ Cryptosystem/Cryptography/Asymmetric/RSA.cs
@@ -1,6 +1,41 @@
-﻿namespace Cryptosystem.Cryptography.Asymmetric;
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
-public class RSA
+namespace Cryptosystem.Cryptography.Asymmetric;
+
+public class LocalRSA
 {
-    
+    private readonly int _keySize;
+    private readonly RSAEncryptionPadding _padding;
+
+    public LocalRSA(int size = 2048, RSAEncryptionPadding? padding = null)
+    {
+        _keySize = size;
+        _padding = padding ?? RSAEncryptionPadding.OaepSHA256;
+    }
+
+    public string Encrypt(string message, string publicKey)
+    {
+        using var provider = RSA.Create(_keySize);
+        
+        provider.ImportFromPem(publicKey);
+
+        var messageBytes = Encoding.Unicode.GetBytes(message);
+        var encryptedBytes = provider.Encrypt(messageBytes, _padding);
+
+        return Convert.ToBase64String(encryptedBytes);
+    }
+
+    public string Decrypt(string encryptedMessage, string privateKey)
+    {
+        using var provider = RSA.Create(_keySize);
+        
+        provider.ImportFromPem(privateKey);
+
+        var encryptedBytes = Convert.FromBase64String(encryptedMessage);
+        var decryptedBytes = provider.Decrypt(encryptedBytes, _padding);
+
+        return Encoding.Unicode.GetString(decryptedBytes);
+    }
 }
---
+++ Cryptosystem/Cryptography/Generators/GenerateRSA.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace Cryptosystem.Cryptography.Generators;
+
+public class GenerateRSA
+{
+    public static (string PublicKeyPEM, string PrivateKeyPEM) PEMKeys(int keySize = 2048)
+    {
+        using var rsa = RSA.Create(keySize);
+        return (
+            rsa.ExportRSAPublicKeyPem(),
+            rsa.ExportPkcs8PrivateKeyPem()
+        );
+    }
+}
